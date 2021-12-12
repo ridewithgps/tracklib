973866bc08d0b12ac915929201c054a2a93b5d9f
Dan Larkin <dan@danlarkin.org>
2021-12-11 16:46:45 -0800

    use CRCWriter in header and metadata modules

--- tracklib2/src/write/header.rs
+++ tracklib2/src/write/header.rs
@@ -1,36 +1,35 @@
-use crate::consts::{CRC16, RWTFMAGIC, RWTF_CREATOR_VERSION, RWTF_FILE_VERSION};
+use super::crcwriter::CrcWriter;
+use crate::consts::{RWTFMAGIC, RWTF_CREATOR_VERSION, RWTF_FILE_VERSION};
 use crate::error::Result;
 use std::io::Write;
 
 #[rustfmt::skip]
-pub(crate) fn write_header<W: Write>(out: &mut W, metadata_table_offset: u16, data_table_offset: u16) -> Result<usize> {
-    let mut buf = Vec::with_capacity(24);
-
-    buf.write_all(&RWTFMAGIC)?;                                    // 8 bytes - Magic Number
-    buf.write_all(&RWTF_FILE_VERSION.to_le_bytes())?;              // 1 byte  - File Version
-    buf.write_all(&[0x00, 0x00, 0x00])?;                           // 3 bytes - FV Reserve
-    buf.write_all(&RWTF_CREATOR_VERSION.to_le_bytes())?;           // 1 byte  - Creator Version
-    buf.write_all(&[0x00, 0x00, 0x00])?;                           // 3 bytes - CV Reserve
-    buf.write_all(&metadata_table_offset.to_le_bytes())?;          // 2 bytes - Offset to Metadata Table
-    buf.write_all(&data_table_offset.to_le_bytes())?;              // 2 bytes - Offset to Data Table
-    buf.write_all(&[0x00, 0x00])?;                                 // 2 bytes - E Reserve
-    buf.write_all(&CRC16.checksum(&buf).to_le_bytes())?;           // 2 bytes - Header CRC
-
-    out.write_all(&buf)?;
-    Ok(buf.len())
+pub(crate) fn write_header<W: Write>(out: &mut W, metadata_table_offset: u16, data_table_offset: u16) -> Result<()> {
+    let mut crcwriter = CrcWriter::new16(out);
+    crcwriter.write_all(&RWTFMAGIC)?;                                    // 8 bytes - Magic Number
+    crcwriter.write_all(&RWTF_FILE_VERSION.to_le_bytes())?;              // 1 byte  - File Version
+    crcwriter.write_all(&[0x00, 0x00, 0x00])?;                           // 3 bytes - FV Reserve
+    crcwriter.write_all(&RWTF_CREATOR_VERSION.to_le_bytes())?;           // 1 byte  - Creator Version
+    crcwriter.write_all(&[0x00, 0x00, 0x00])?;                           // 3 bytes - CV Reserve
+    crcwriter.write_all(&metadata_table_offset.to_le_bytes())?;          // 2 bytes - Offset to Metadata Table
+    crcwriter.write_all(&data_table_offset.to_le_bytes())?;              // 2 bytes - Offset to Data Table
+    crcwriter.write_all(&[0x00, 0x00])?;                                 // 2 bytes - E Reserve
+    crcwriter.append_crc()?;                                             // 2 bytes - Header CRC
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use assert_matches::assert_matches;
 
     #[test]
     fn test_write_header() {
         let mut buf = vec![];
-        let written = write_header(&mut buf, 0x0A, 0x1A);
-        assert!(written.is_ok());
-        #[rustfmt::skip]
-        let expected = &[0x89, // magic number
+        assert_matches!(write_header(&mut buf, 0x0A, 0x1A), Ok(()) => {
+            #[rustfmt::skip]
+            assert_eq!(buf,
+                       &[0x89, // magic number
                          0x52,
                          0x57,
                          0x54,
@@ -53,8 +52,7 @@ mod tests {
                          0x00, // e reserve
                          0x00,
                          0x86, // header crc
-                         0x76];
-        assert_eq!(buf, expected);
-        assert_eq!(written.unwrap(), expected.len());
+                         0x76]);
+        });
     }
 }
--- tracklib2/src/write/metadata.rs
+++ tracklib2/src/write/metadata.rs
@@ -1,82 +1,73 @@
-use crate::consts::CRC16;
-use crate::error::{Result, TracklibError};
+use super::crcwriter::CrcWriter;
+use crate::error::Result;
 use crate::types::{MetadataEntry, TrackType};
 use std::convert::TryFrom;
 use std::io::Write;
 
 impl MetadataEntry {
-    fn write(&self) -> Result<(u8, Vec<u8>)> {
+    #[rustfmt::skip]
+    fn write<W: Write>(&self, out: &mut W) -> Result<()> {
         match self {
             Self::TrackType(track_type) => {
-                // TrackType metadata is 5 bytes
-                // 1 byte for the type of track
-                // 4 bytes for the id of the track
                 let (type_tag, id): (u8, &u32) = match track_type {
                     TrackType::Trip(id) => (0x00, id),
                     TrackType::Route(id) => (0x01, id),
                     TrackType::Segment(id) => (0x02, id),
                 };
-                let mut buf = Vec::with_capacity(5);
-                buf.write_all(&type_tag.to_le_bytes())?;
-                buf.write_all(&id.to_le_bytes())?;
-                Ok((0x00, buf))
+                out.write_all(&[0x00])?;                            // 1 byte  - entry type
+                out.write_all(&[0x05, 0x00])?;                      // 2 bytes - entry size
+                out.write_all(&type_tag.to_le_bytes())?;            // 1 byte  - TrackType type tag
+                out.write_all(&id.to_le_bytes())?;                  // 4 bytes - TrackType id
+                Ok(())
             }
             Self::CreatedAt(seconds_since_epoch) => {
-                // CreatedAt metadata is 8 bytes: seconds since epoch
-                Ok((0x01, seconds_since_epoch.to_le_bytes().to_vec()))
+                out.write_all(&[0x01])?;                            // 1 byte  - entry type
+                out.write_all(&[0x08, 0x00])?;                      // 2 bytes - entry size
+                out.write_all(&seconds_since_epoch.to_le_bytes())?; // 8 bytes - created_at
+                Ok(())
             }
         }
     }
 }
 
 #[rustfmt::skip]
-pub(crate) fn write_metadata<W: Write>(out: &mut W, entries: &[MetadataEntry]) -> Result<usize> {
-    let entry_count = u8::try_from(entries.len())?;
+pub(crate) fn write_metadata<W: Write>(out: &mut W, entries: &[MetadataEntry]) -> Result<()> {
+    let mut crcwriter = CrcWriter::new16(out);
 
-    let mut buf = Vec::new();
-
-    buf.write_all(&entry_count.to_le_bytes())?;                    // 1 byte  - entry count
-
+    crcwriter.write_all(&u8::try_from(entries.len())?.to_le_bytes())?; // 1 byte  - entry count
     for entry in entries {
-        let (entry_type, entry_bytes) = entry.write()?;
-        let entry_size = u16::try_from(entry_bytes.len())?;
-
-        buf.write_all(&entry_type.to_le_bytes())?;                 // 1 byte  - entry type
-        buf.write_all(&entry_size.to_le_bytes())?;                 // 2 bytes - entry size
-        buf.write_all(&entry_bytes)?;                              // ? bytes - entry value
+        entry.write(&mut crcwriter)?;                                  // ? bytes - entry contents
     }
+    crcwriter.append_crc()?;                                           // 2 bytes - crc
 
-    buf.write_all(&CRC16.checksum(&buf).to_le_bytes())?;           // 2 bytes - crc
-
-    out.write_all(&buf)?;
-    Ok(buf.len())
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use assert_matches::assert_matches;
     use std::time::Duration;
 
     #[test]
     fn test_write_empty_metadata() {
         let mut buf = vec![];
-        let written = write_metadata(&mut buf, &[]);
-        assert!(written.is_ok());
-        #[rustfmt::skip]
-        let expected = &[0x00, // zero metadata entries
+        assert_matches!(write_metadata(&mut buf, &[]), Ok(()) => {
+            #[rustfmt::skip]
+            assert_eq!(buf,
+                       &[0x00, // zero metadata entries
                          0x40, // crc
-                         0xBF];
-        assert_eq!(buf, expected);
-        assert_eq!(written.unwrap(), expected.len());
+                         0xBF]);
+        });
     }
 
     #[test]
     fn test_only_track_type_trip() {
         let mut buf = vec![];
-        let written = write_metadata(&mut buf, &[MetadataEntry::TrackType(TrackType::Trip(400))]);
-        assert!(written.is_ok());
-        #[rustfmt::skip]
-        let expected = &[0x01, // one metadata entry
+        assert_matches!(write_metadata(&mut buf, &[MetadataEntry::TrackType(TrackType::Trip(400))]), Ok(()) => {
+            #[rustfmt::skip]
+            assert_eq!(buf,
+                       &[0x01, // one metadata entry
                          0x00, // entry type: track_type = 0x00
                          0x05, // two byte entry size = 5
                          0x00,
@@ -86,18 +77,17 @@ mod tests {
                          0x00,
                          0x00,
                          0xD1, // crc
-                         0x5F];
-        assert_eq!(buf, expected);
-        assert_eq!(written.unwrap(), expected.len());
+                         0x5F]);
+        });
     }
 
     #[test]
     fn test_only_track_type_route() {
         let mut buf = vec![];
-        let written = write_metadata(&mut buf, &[MetadataEntry::TrackType(TrackType::Route(64))]);
-        assert!(written.is_ok());
-        #[rustfmt::skip]
-        let expected = &[0x01, // one metadata entry
+        assert_matches!(write_metadata(&mut buf, &[MetadataEntry::TrackType(TrackType::Route(64))]), Ok(()) => {
+            #[rustfmt::skip]
+            assert_eq!(buf,
+                       &[0x01, // one metadata entry
                          0x00, // entry type: track_type = 0x00
                          0x05, // two byte entry size = 5
                          0x00,
@@ -107,42 +97,36 @@ mod tests {
                          0x00,
                          0x00,
                          0x85, // crc
-                         0x9F];
-        assert_eq!(buf, expected);
-        assert_eq!(written.unwrap(), expected.len());
+                         0x9F]);
+        });
     }
 
     #[test]
     fn test_only_track_type_segment() {
         let mut buf = vec![];
-        let written = write_metadata(
-            &mut buf,
-            &[MetadataEntry::TrackType(TrackType::Segment(u32::MAX))],
-        );
-        assert!(written.is_ok());
-        #[rustfmt::skip]
-        let expected = &[0x01, // one metadata entry
-                         0x00, // entry type: track_type = 0x00
-                         0x05, // two byte entry size = 5
-                         0x00,
-                         0x02, // track type: segment = 0x02
-                         0xFF, // four byte segment ID = 4,294,967,295
-                         0xFF,
-                         0xFF,
-                         0xFF,
-                         0xD5, // crc
-                         0xCB];
-        assert_eq!(buf, expected);
-        assert_eq!(written.unwrap(), expected.len());
+        assert_matches!(write_metadata(&mut buf, &[MetadataEntry::TrackType(TrackType::Segment(u32::MAX))]), Ok(()) => {
+            #[rustfmt::skip]
+            assert_eq!(buf, &[0x01, // one metadata entry
+                              0x00, // entry type: track_type = 0x00
+                              0x05, // two byte entry size = 5
+                              0x00,
+                              0x02, // track type: segment = 0x02
+                              0xFF, // four byte segment ID = 4,294,967,295
+                              0xFF,
+                              0xFF,
+                              0xFF,
+                              0xD5, // crc
+                              0xCB]);
+        });
     }
 
     #[test]
     fn test_only_created_at_epoch() {
         let mut buf = vec![];
-        let written = write_metadata(&mut buf, &[MetadataEntry::CreatedAt(0)]);
-        assert!(written.is_ok());
-        #[rustfmt::skip]
-        let expected = &[0x01, // one metadata entry
+        assert_matches!(write_metadata(&mut buf, &[MetadataEntry::CreatedAt(0)]), Ok(()) => {
+            #[rustfmt::skip]
+            assert_eq!(buf,
+                       &[0x01, // one metadata entry
                          0x01, // entry type: created_at = 0x01
                          0x08, // two byte entry size = 8
                          0x00,
@@ -155,19 +139,18 @@ mod tests {
                          0x00,
                          0x00,
                          0xE3, // crc
-                         0x28];
-        assert_eq!(buf, expected);
-        assert_eq!(written.unwrap(), expected.len());
+                         0x28]);
+        });
     }
 
     #[test]
     fn test_only_created_at_future() {
         let mut buf = vec![];
         let the_future = Duration::from_millis(u64::MAX).as_secs();
-        let written = write_metadata(&mut buf, &[MetadataEntry::CreatedAt(the_future)]);
-        assert!(written.is_ok());
-        #[rustfmt::skip]
-        let expected = &[0x01, // one metadata entry
+        assert_matches!(write_metadata(&mut buf, &[MetadataEntry::CreatedAt(the_future)]), Ok(()) => {
+            #[rustfmt::skip]
+            assert_eq!(buf,
+                       &[0x01, // one metadata entry
                          0x01, // entry type: created_at = 0x01
                          0x08, // two byte entry size = 8
                          0x00,
@@ -180,63 +163,50 @@ mod tests {
                          0x41,
                          0x00,
                          0x21, // crc
-                         0x4C];
-        assert_eq!(buf, expected);
-        assert_eq!(written.unwrap(), expected.len());
+                         0x4C]);
+        });
     }
 
     #[test]
     fn test_both() {
         let mut buf = vec![];
-        let written = write_metadata(
-            &mut buf,
-            &[
-                MetadataEntry::TrackType(TrackType::Trip(20)),
-                MetadataEntry::CreatedAt(0),
-            ],
-        );
-        assert!(written.is_ok());
-        #[rustfmt::skip]
-        let expected = &[0x02, // two metadata entries
-                         0x00, // entry type: track_type = 0x00
-                         0x05, // two byte entry size = 5
-                         0x00,
-                         0x00, // track type: trip = 0x00
-                         0x14, // four byte trip ID = 20
-                         0x00,
-                         0x00,
-                         0x00,
-                         0x01, // entry type: created_at = 0x01
-                         0x08, // two byte entry size = 8
-                         0x00,
-                         0x00, // eight byte timestamp: zero seconds elapsed
-                         0x00,
-                         0x00,
-                         0x00,
-                         0x00,
-                         0x00,
-                         0x00,
-                         0x00,
-                         0x23, // crc
-                         0xD2];
-        assert_eq!(buf, expected);
-        assert_eq!(written.unwrap(), expected.len());
+        assert_matches!(write_metadata(&mut buf, &[MetadataEntry::TrackType(TrackType::Trip(20)),
+                                                   MetadataEntry::CreatedAt(0)]), Ok(()) => {
+            #[rustfmt::skip]
+            assert_eq!(buf, &[0x02, // two metadata entries
+                              0x00, // entry type: track_type = 0x00
+                              0x05, // two byte entry size = 5
+                              0x00,
+                              0x00, // track type: trip = 0x00
+                              0x14, // four byte trip ID = 20
+                              0x00,
+                              0x00,
+                              0x00,
+                              0x01, // entry type: created_at = 0x01
+                              0x08, // two byte entry size = 8
+                              0x00,
+                              0x00, // eight byte timestamp: zero seconds elapsed
+                              0x00,
+                              0x00,
+                              0x00,
+                              0x00,
+                              0x00,
+                              0x00,
+                              0x00,
+                              0x23, // crc
+                              0xD2]);
+        });
     }
 
     #[test]
     fn test_duplicate_types() {
         let mut buf = vec![];
-        let written = write_metadata(
-            &mut buf,
-            &[
-                MetadataEntry::TrackType(TrackType::Trip(20)),
-                MetadataEntry::TrackType(TrackType::Trip(21)),
-                MetadataEntry::TrackType(TrackType::Route(22)),
-            ],
-        );
-        assert!(written.is_ok());
-        #[rustfmt::skip]
-        let expected = &[0x03, // three metadata entries
+        assert_matches!(write_metadata(&mut buf, &[MetadataEntry::TrackType(TrackType::Trip(20)),
+                                                   MetadataEntry::TrackType(TrackType::Trip(21)),
+                                                   MetadataEntry::TrackType(TrackType::Route(22))]), Ok(()) => {
+            #[rustfmt::skip]
+            assert_eq!(buf,
+                       &[0x03, // three metadata entries
                          0x00, // entry type: track_type = 0x00
                          0x05, // two byte entry size = 5
                          0x00,
@@ -262,8 +232,7 @@ mod tests {
                          0x00,
                          0x00,
                          0xDE, // crc
-                         0x57];
-        assert_eq!(buf, expected);
-        assert_eq!(written.unwrap(), expected.len());
+                         0x57]);
+        });
     }
 }

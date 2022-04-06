d93dbeba56c6702ab75ac357bf97a67429f04759
Dan Larkin <dan@danlarkin.org>
2022-04-06 12:42:43 -0700

    ruby lib: rename crate::track_reader to crate::read

--- ruby_tracklib2/src/lib.rs
+++ ruby_tracklib2/src/lib.rs
@@ -1,5 +1,5 @@
 mod schema;
-mod track_reader;
+mod read;
 mod write;
 use rutie::{Module, Object};
 
@@ -10,16 +10,16 @@ pub extern "C" fn Init_Tracklib() {
         module
             .define_nested_class("TrackReader", None)
             .define(|class| {
-                class.def_self("new", track_reader::trackreader_new);
-                class.def("metadata", track_reader::trackreader_metadata);
-                class.def("file_version", track_reader::trackreader_file_version);
-                class.def("creator_version", track_reader::trackreader_creator_version);
+                class.def_self("new", read::trackreader_new);
+                class.def("metadata", read::trackreader_metadata);
+                class.def("file_version", read::trackreader_file_version);
+                class.def("creator_version", read::trackreader_creator_version);
 
-                class.def("section_count", track_reader::trackreader_section_count);
-                class.def("section_encoding", track_reader::trackreader_section_encoding);
-                class.def("section_schema", track_reader::trackreader_section_schema);
-                class.def("section_rows", track_reader::trackreader_section_rows);
-                class.def("section_data", track_reader::trackreader_section_data);
+                class.def("section_count", read::trackreader_section_count);
+                class.def("section_encoding", read::trackreader_section_encoding);
+                class.def("section_schema", read::trackreader_section_schema);
+                class.def("section_rows", read::trackreader_section_rows);
+                class.def("section_data", read::trackreader_section_data);
             });
 
         module.define_nested_class("Section", None).define(|class| {

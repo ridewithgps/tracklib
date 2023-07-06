1f66595bb641d2ae24151806a0273eb27ca4532f
Dan Larkin <dan@danlarkin.org>
2023-07-06 16:28:24 -0700

    add conversion methods to tracklib::types::FieldValue

--- tracklib/src/types.rs
+++ tracklib/src/types.rs
@@ -10,6 +10,72 @@ pub enum FieldValue {
     ByteArray(Vec<u8>),
 }
 
+impl FieldValue {
+    pub fn into_i64(self) -> Option<i64> {
+        if let Self::I64(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    pub fn into_u64(self) -> Option<u64> {
+        if let Self::U64(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    pub fn into_f64(self) -> Option<f64> {
+        if let Self::F64(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    pub fn into_bool(self) -> Option<bool> {
+        if let Self::Bool(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    pub fn into_string(self) -> Option<String> {
+        if let Self::String(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    pub fn into_bool_array(self) -> Option<Vec<bool>> {
+        if let Self::BoolArray(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    pub fn into_u64_array(self) -> Option<Vec<u64>> {
+        if let Self::U64Array(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    pub fn into_byte_array(self) -> Option<Vec<u8>> {
+        if let Self::ByteArray(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+}
+
 #[derive(Debug, PartialEq)]
 pub enum TrackType {
     Trip(u64),
